47e898112c070093def22f2763a43328f6074351
rishabh1402 <75234829+rishabh1402@users.noreply.github.com>
2021-10-30 23:43:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,25 +8,23 @@ UPLOAD_PATH = os.path.join(BASE_PATH,'static/upload/')
 
 def get_frame(path,filename):
     # face detector initialized   
-        faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-        # get/read the image
-        img = cv2.imread(path)
-        # print(img)  # check if image read successfully
+    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    # get/read the image
+    img = cv2.imread(path)
+    # print(img)  # check if image read successfully
+    # detect face by decreasing the scaleFactor by 1.05
+    faces = faceCascade.detectMultiScale(img, scaleFactor=1.05, minNeighbors=5)
+    # initial no. of faces = 0
+    count = 0
+    # draw a rectangle around the detected face
+    for (x, y, w, h) in faces:
+        # draw rectangle around face
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # increment face count upon detection
+        count += 1
 
-        # detect face by decreasing the scaleFactor by 1.05
-        faces = faceCascade.detectMultiScale(img, scaleFactor=1.05, minNeighbors=5)
-
-        # initial no. of faces = 0
-        count = 0
-        # draw a rectangle around the detected face
-        for (x, y, w, h) in faces:
-            # draw rectangle around face
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            # increment face count upon detection
-            count += 1
-
-        text = cv2.putText(img, "Total Faces Detected : " + str(count), (45, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        return text
+    text = count
+    return text
          
     
 
